itsloopyo/pacific-drive-headtracking

refactor(smoothing): take the defaults from core instead of restating them
A hardcoded 0.0/0.15 means a change in core silently disagrees with the
mod that pins it. These now reference kDefaultLocalSmoothing and
kDefaultRemoteSmoothing so the value has one home.

Claude-Session: https://claude.ai/code/session_01XM4VdKookexD6HQXQYHhnF

src/config.h
@@ -5,6 +5,9 @@
 #include <cstdint>
 #include <string>
 
+#include "cameraunlock/data/position_settings.h"
+#include "cameraunlock/math/smoothing_utils.h"
+
 namespace pdht {
 
 // Mod configuration, loaded from PacificDriveHeadTracking.ini next to the
@@ -27,25 +30,25 @@ struct Config {
     // Smoothing is chosen per connection: local for a tracker on this machine
     // (loopback), remote for a device on the network. Both cover rotation and
     // position.
-    float localSmoothing = 0.0f;
-    float remoteSmoothing = 0.15f;
+    float localSmoothing = static_cast<float>(cameraunlock::math::kDefaultLocalSmoothing);
+    float remoteSmoothing = static_cast<float>(cameraunlock::math::kDefaultRemoteSmoothing);
 
     // [Position]
     bool positionEnabled = true;
     float positionSensitivityX = 1.0f;
     float positionSensitivityY = 1.0f;
     float positionSensitivityZ = 1.0f;
-    float limitX = 0.30f;
+    float limitX = cameraunlock::PositionSettings{}.limit_x;
     // The vertical clamp is [-limitYDown, +limitY]. Two fields, because a
     // tighter crouch range than standing range is a real thing to want; when
     // LimitYDown is absent it mirrors LimitY, which is the symmetric case.
     // Leaving limit_y_down at the core struct's own default instead meant a user
     // who set LimitY=0.05 still got 0.20 m of downward travel, with nothing in
     // the log or the docs saying the key was only half-effective.
-    float limitY = 0.20f;
-    float limitYDown = 0.20f;
-    float limitZ = 0.40f;
-    float limitZBack = 0.10f;
+    float limitY = cameraunlock::PositionSettings{}.limit_y;
+    float limitYDown = cameraunlock::PositionSettings{}.limit_y_down;
+    float limitZ = cameraunlock::PositionSettings{}.limit_z;
+    float limitZBack = cameraunlock::PositionSettings{}.limit_z_back;
 
     // [Camera]
     // Degrees added to the game's own horizontal field of view. Pacific Drive

itsloopyo/pinball-fx-headtracking

src/config.h
@@ -5,6 +5,8 @@
 
 #include <string>
 
+#include "cameraunlock/math/smoothing_utils.h"
+
 namespace pinballfx_ht
 {
     struct Config
@@ -25,8 +27,8 @@ namespace pinballfx_ht
 
         // Two smoothing parameters, picked per connection from the packet source
         // address. Both cover rotation and position.
-        float local_smoothing = 0.0f;
-        float remote_smoothing = 0.15f;
+        float local_smoothing = static_cast<float>(cameraunlock::math::kDefaultLocalSmoothing);
+        float remote_smoothing = static_cast<float>(cameraunlock::math::kDefaultRemoteSmoothing);
 
         // Camera field of view, in degrees. The game exposes no FOV control, so
         // these are the only way to change it. fov_override replaces whatever